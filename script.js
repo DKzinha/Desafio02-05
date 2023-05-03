fd679989ae69d7afe6da2d8c6663af70a8615798
unknown <dkrochamartins@gmail.com>
2023-05-03 13:33:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var texto;
+function soma(){
+var a = parseInt(document.querySelector("#n1").value);
+var b = parseInt(document.querySelector("#n2").value) 
+var soma = a + b;
+        document.getElementById("soma").innerHTML = "O resultado  é: " + soma;
+}
